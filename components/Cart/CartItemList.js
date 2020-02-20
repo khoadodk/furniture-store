064c54976e34b0d041fb4813dd1af9a770eb04ce
Khoa Do <khoado.dk@gmail.com>
2020-02-20 13:30:25 -0500

    Complete UI cart component

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,23 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react';
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = true;
+
+  return (
+    <Segment secondary color="blue" inverted textAlign="center">
+      <Header icon>
+        <Icon name="shopping beasket" />
+        No products in your cart. Keep Shopping!
+      </Header>
+      <div>
+        {user ? (
+          <Button color="teal">View Products</Button>
+        ) : (
+          <Button color="teal">Login to Add Products</Button>
+        )}
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,15 @@
+import { Button, Segment, Divider } from 'semantic-ui-react';
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return (
+    <>
+      <Divider />
+      <Segment clearing size="large">
+        <strong>Sub Total:</strong> $0.00
+        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+      </Segment>
+    </>
+  );
 }
 
 export default CartSummary;
